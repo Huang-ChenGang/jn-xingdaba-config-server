8b9739160c410f9948d5e718fbd82d05b4fb99df
Huang ChenGang <hcggang@163.com>
2020-12-26 10:24:22 +0800

    init project

---
+++ src/main/java/com/jn/xingdaba/config/JnXingdabaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnXingdabaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnXingdabaConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jn/xingdaba/config/JnXingdabaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnXingdabaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
